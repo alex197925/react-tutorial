d3efb305971d05006991c66aa94ded52187edd17
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-05 10:56:30 +0200

    Passing data from App.js to ExpenseItem.js  component with props

--- tutorial-app/src/components/ExpenseItem.js
+++ tutorial-app/src/components/ExpenseItem.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2022, 8, 23);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 298.76;
+    // const expenseDate = new Date(2022, 8, 23);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 298.76;
     return (
         <div className="expense-item ">
-           <div>{expenseDate.toISOString()}</div>
+           <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price"> $ {expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price"> $ {props.amount}</div>
             </div>
         </div>
     );
