e8d0cb534f1b65a23a5b7a51af78f02c038bc828
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-05 10:31:13 +0200

    Add expenseDate,expenseTitle,expenseAmount

--- tutorial-app/src/components/ExpenseItem.js
+++ tutorial-app/src/components/ExpenseItem.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const expenseDate = new Date(2022, 8, 23);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 298.76;
     return (
         <div className="expense-item ">
-           <div>March 29th 2022</div>
+           <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price"> $ 298.76</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price"> $ {expenseAmount}</div>
             </div>
         </div>
     );
