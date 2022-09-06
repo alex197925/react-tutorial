ee6b87b02cabc38ede8631949547af9854a41cc2
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-06 15:05:20 +0200

    Pass data from App.js to Expenses.js component

--- tutorial-app/src/App.js
+++ tutorial-app/src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 
 function App() {
@@ -25,24 +26,10 @@ function App() {
     ];
     return (
         <div className="app">
-            <ExpenseItem
-                title={expenses[0].title}
-                date={expenses[0].date}
-                amount={expenses[0].amount}/>
-            <ExpenseItem
-                title={expenses[1].title}
-                date={expenses[1].date}
-                amount={expenses[1].amount}/>
-            <ExpenseItem
-                title={expenses[2].title}
-                date={expenses[2].date}
-                amount={expenses[2].amount}/>
-            <ExpenseItem
-                title={expenses[3].title}
-                date={expenses[3].date}
-                amount={expenses[3].amount}/>
+          <Expenses items={expenses}/>
         </div>
     );
 }
+
 
 export default App;
