f3b1e696b8ac22b70dc946844b6f8d32e5e73bb1
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-06 15:52:00 +0200

    Learning the concept of Composition children props

--- tutorial-app/src/components/Expenses.js
+++ tutorial-app/src/components/Expenses.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "./Card";
 
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem
                 title={props.items[0].title}
                 date={props.items[0].date}
@@ -20,7 +22,7 @@ function Expenses(props) {
                 title={props.items[3].title}
                 date={props.items[3].date}
                 amount={props.items[3].amount}/>
-        </div>
+        </Card>
     );
 }
 
