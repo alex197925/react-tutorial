13bcec95c7f436c9661dac46d7539033f592c8b9
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-14 16:43:45 +0200

    In Expenses.js maping through all items-expenses and create ExpensesItem component and displaying

--- tutorial-app/src/components/Expenses/Expenses.js
+++ tutorial-app/src/components/Expenses/Expenses.js
@@ -20,23 +20,11 @@ const Expenses = (props) => {
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selected={year} onselectYear={filterChangeHandler} />
-                <ExpenseItem
-                    title={props.items[0].title}
-                    date={props.items[0].date}
-                    amount={props.items[0].amount}/>
-                <ExpenseItem
-                    title={props.items[1].title}
-                    date={props.items[1].date}
-                    amount={props.items[1].amount}/>
-                <ExpenseItem
-                    title={props.items[2].title}
-                    date={props.items[2].date}
-                    amount={props.items[2].amount}/>
-                <ExpenseItem
-                    title={props.items[3].title}
-                    date={props.items[3].date}
-                    amount={props.items[3].amount}/>
+                <ExpensesFilter selected={year} onselectYear={filterChangeHandler}/>
+                {/*Map through all items-expenses and create ExpensesItem component*/}
+                {props.items.map((expense) => <ExpenseItem title={expense.title} date={expense.date}
+                                                           amount={expense.amount} key={expense.id}/>)}
+
             </Card>
         </div>
 
