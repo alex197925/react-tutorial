63230febe7e7f62b62495a0dedc1381c6f6b4a45
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-23 11:22:13 +0200

    Add Chart.js ChartBar.js

---
+++ tutorial-app/src/components/Chart/ChartBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ChartBar.css';
+
+function ChartBar(props) {
+
+    // Set height of fill bar set to 0%
+    let barFillHeight = '0%';
+    // Check filled percentage of the bar and getting percentage from 0 - 100
+    if(props.max > 0) {
+        barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+    }
+    return (
+        <div className="chart-bar">
+            <div className="chart-bar__inner">
+                <div className="chart-bar__fill" style={{height: barFillHeight}}></div>
+            </div>
+            <div className="chart-bar__label">{props.lable}</div>
+        </div>
+    );
+}
+
+export default ChartBar;
---
+++ tutorial-app/src/components/Expenses/ExpensesCart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chart from "../Chart/Chart";
+
+function ExpensesCart(props) {
+    return (
+        <div>
+            <Chart/>
+        </div>
+    );
+}
+
+export default ExpensesCart;
