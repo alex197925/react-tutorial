d5ad0e71c283c44384eacc3807731cce2c0938b5
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-23 14:09:14 +0200

    Add props

--- tutorial-app/src/components/Chart/Chart.js
+++ tutorial-app/src/components/Chart/Chart.js
@@ -3,12 +3,15 @@ import './Chart.css';
 import ChartBar from "./ChartBar";
 
 function Chart(props) {
+    const dataPointValues = props.dataPoints.map(dataPint => dataPint.value);
+    const totalMaximum = Math.max(...dataPointValues);
     return (
         <div className="chart">
-            {props.dataPoints.map(dataPoint => <ChartBar
+            {props.dataPoints.map((dataPoint) =>
+                <ChartBar
                 key={dataPoint.label}
                 value={dataPoint.value}
-                maxValue={null}
+                maxValue={totalMaximum}
                 lable={dataPoint.label}/>)}
         </div>
     );
